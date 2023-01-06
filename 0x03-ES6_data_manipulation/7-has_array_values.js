c48a129da1b84a2456e8142725c7e1604ca51700
Oyizaadewuyi <oyizaadewuyi@gmail.com>
2023-01-06 23:08:50 +0000

    Data manipulation

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,6 @@
-export default function hasValuesFromArray(set, array) {
-  return array.every((value) => set.has(value));
-}
+export default function
+hasValuesFromArray(set, array) {
+
+  return array.every((value) =>
+set.has(value));
 }
